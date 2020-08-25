b549810c934f9b8c692199f42d5b14b9e44e7f47
CWillis02 <69876818+CWillis02@users.noreply.github.com>
2020-08-24 20:31:59 -0500

    Initial commit

---
+++ hello (3).c
@@ -0,0 +1,15 @@
+/**
+ * Author: Christopher Willis
+ * Date:   2020
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Christopher Willis\n");
+     
+     return 0;
+ }
